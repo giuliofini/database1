d9e21c173a2a163ef3b573765b7886ba5fc77e9f
giulio <giuliofini08@gmail.com>
2018-09-22 14:31:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/giulf/database/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.giulf.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button avanti = (Button) findViewById(R.id.avanti);
+        
+    }
+}
